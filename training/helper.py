5ca6ad22797b863296b80f76e552ae53bc949242
iTz-Shiny <itzdarkvoid@gmail.com>
2019-04-17 14:50:34 -0700

    training data
    Added the .csv files for the initial training data alongside with a helper python program that converts NED .txt file into a .CSV file that SDSS CROSSID can use.

---
+++ training/helper.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 16:40:42 2019
+
+@author: Steven Li
+"""
+
+import csv
+import os
+import sys
+
+#Setup
+perseus = []
+background = []
+
+#Functions
+def find_nth(line,char,n):
+    start = line.find(char)
+    while start >= 0 and n > 1:
+        start = line.find(char , start+len(char))
+        n -= 1
+    return start
+
+def extract(path):
+    arr = []
+    with open(path) as file:
+        ln = file.readline()
+        while ln and len(ln) > 1:
+            name_start = find_nth(ln,'|',1)
+            ra_start = find_nth(ln,'|',2)
+            dec_start = find_nth(ln,'|',3)
+            stop = find_nth(ln,'|',4)
+            name = ln[name_start+2:ra_start-1].replace(" ","")
+            ra = ln[ra_start+2:dec_start-1].replace(" ","")
+            dec = ln[dec_start+2:stop-1].replace(" ","")
+            arr.append([name,ra,dec])
+            ln = file.readline()
+    arr.pop() #Throw away last empty
+    return arr
+
+def write(arr,name):
+    with open(name,"w+", newline='') as my_csv:
+        csvWriter = csv.writer(my_csv)
+        csvWriter.writerows(arr)
+        
+def main():
+    print('Converts NED .txt file in Name, RA, DEC .csv file. ')
+    print('Input File Path: ')
+    filepath = input().replace(" ","")
+    print('Input New CSV name: ')
+    filename = input().replace(" ","")
+
+    if not os.path.isfile(filepath):
+       print("File path {} does not exist. Exiting...".format(filepath))
+       exit
+     
+    array = extract(filepath)
+      
+    if os.path.isfile(filename+".csv"):
+        print("File exists, overwrite (Y/N)?")
+        x = input().lower()
+        if ((x == 'y') or (x == 'yes')):
+            if(write(array,filename+".csv")):
+                print("{}.csv created succesfully.".format(filename))
+            else:
+                print("{}.csv created unsuccesfully.".format(filename))
+        else:
+            exit
+
+if __name__ == '__main__':  
+   main()
